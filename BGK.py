2668e9eaed5b3693d1d38c424fdcc6e37f4b5205
afr00x <109451961+afr00x@users.noreply.github.com>
2022-07-17 12:43:26 +0700

    Add files via upload

---
+++ BGK.py
@@ -0,0 +1,61 @@
+import random
+from secrets import choice
+from turtle import color, delay
+
+print("Selamat Datang di Game AFRAAAA")
+
+
+color
+while True: 
+    choices = ["batu","gunting","kertas"]
+
+    computer = random.choice(choices)
+    player = None
+
+    while player not in choices:
+        player = input("batu,gunting,kertas?:").lower()
+
+    if player == computer:
+        print("computer:",computer)
+        print("player:",player)
+        print("Seri!")
+
+    elif player == "batu":
+        if computer == "kertas":
+            print("computer:",computer)
+            print("player:",player)
+            print("Kalah CUPU !!!")
+        if computer == "gunting":
+            print("computer:",computer)
+            print("player",player)
+            print("Anjaayyy Menang COkkk")
+
+    elif player == "gunting":
+        if computer == "batu":
+            print("computer:",computer)
+            print("player",player)
+            print("Kalah CUPU !!!")
+        if computer == "kertas":
+            print("computer:",computer)
+            print("player:",player)
+            print("Anjaayyy Menang COkkk")
+
+    elif player == "kertas":
+        if computer == "gunting":
+            print("computer:",computer)
+            print("player",player)
+            print("Kalah CUPU !!!")
+        if computer == "batu":
+            print("computer:",computer)
+            print("player:",player)
+            print("Anjaayyy Menang COkkk")
+
+
+    play_again = input("Main lagi? (gas/gak):").lower()
+
+    if play_again !="gas":
+       break
+print("oke byee")    
+delay("3000")
+# ****************************************************************
+
